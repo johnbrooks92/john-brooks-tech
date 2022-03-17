19d184ed2172f86abc2212d68f6f69c8b4b4030a
John <johntbrooks92@gmail.com>
2022-03-17 00:33:55 -0500

    fix change

--- Tech.Web/Startup.cs
+++ Tech.Web/Startup.cs
@@ -35,7 +35,7 @@ namespace Tech.Web
             }
             else
             {
-                app.UseExceptionHandler("/Home/Index");
+                app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
