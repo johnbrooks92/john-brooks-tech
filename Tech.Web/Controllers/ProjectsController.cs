e7615bb1b5e8dec0ec1ee07e73d8dda412e76176
John <johntbrooks92@gmail.com>
2022-04-13 16:35:18 -0500

    Project Page Styling Mobile Responsive

--- Tech.Web/Controllers/ProjectsController.cs
+++ Tech.Web/Controllers/ProjectsController.cs
@@ -18,7 +18,7 @@ namespace Tech.Web.Controllers
             {
                 Route = "https://sahelpinghands.org",
                 Name = "Helping Hands",
-                Description = "Capstone project rendered utilizing SpringBoot, Java, Spring Security, Thymeleaf and SASS/SCSS for styling.",
+                Description = "Full-Stack Capstone project rendered utilizing SpringBoot, Java, Spring Security, Thymeleaf and SASS/SCSS for styling.",
                 Type = "Donation Forum/Database Rendering",
                 Creator = "Lening Rodas, Christian Parker, Evan Mead, John Brooks"
 
@@ -28,7 +28,7 @@ namespace Tech.Web.Controllers
             {
                 Route = "/weathermap/weather_map.html",
                 Name = "WeatherNova",
-                Description = "Weather app made using API calls to the OpenWeatherMap API; Rendering instant 7-day forecast upon location selection as well as a geomap to visually represent the data.",
+                Description = "Weather app made using API calls to the OpenWeatherMap API; Renders instant 7-day forecast upon location selection.",
                 Type = "API Calls and MapRendering",
                 Creator = "John Brooks"
 
@@ -37,8 +37,8 @@ namespace Tech.Web.Controllers
             vm.Projects.Add(new Project()
             {
                 Route = "/nemesis-engage-2/index.html",
-                Name = "Nemesis Engage 2",
-                Description = "Turn-based fighting system demo created utilizing HTML, CSS, JS and jQuery. 2.0 version includes expanded roster from 2 to 30 characters and utilizing JSON object for enhanced fighting mechanics.",
+                Name = "NE: 2",
+                Description = "Turn-based fighting system demo created utilizing HTML, CSS, JS and jQuery. Expanded roster from 2 to 30 characters utilizing JSON object for enhanced fighting mechanics.",
                 Type = "Video Game",
                 Creator = "John Brooks"
 
@@ -46,12 +46,32 @@ namespace Tech.Web.Controllers
             
             vm.Projects.Add(new Project()
             {
-                Route = "/pizza-app/order-pizza.html",
-                Name = "Pizza Ordering App",
-                Description = "Pizza ordering app built using fully-static structure of HTML, CSS, and JS.",
-                Type = "Static Ordering Cart",
-                Creator = "John Brooks"
+                Route = "https://github.com/SFW-FreeDevelopment/LordOfFrames",
+                Name = "Lord Of Frames",
+                Description = "AI Bot created for use in Discord Server using .NET 6/C#. Outputs instant data responses for real-time command requests.",
+                Type = "Machine Programming",
+                Creator = "Christopher Plummer, John Brooks"
 
+            });
+            
+            vm.Projects.Add(new Project()
+            {
+                Route = "/movie-app/index.html",
+                Name = "Six Degrees of Sandler",
+                Description = "API Requests using IMDB to enable CRUD, Search functionality within local database.",
+                Type = "API/Querying Functionality",
+                Creator = "John Brooks, Christian Parker"
+            
+            });
+            
+            vm.Projects.Add(new Project()
+            {
+                Route = "/coffee-project/index.html",
+                Name = "Boom! Roasted",
+                Description = "Coffee Ordering Project. Michael Scott-themed.",
+                Type = "Static Ordering Functionality",
+                Creator = "John Brooks, Connor Wulf"
+            
             });
 
             return View(vm);
